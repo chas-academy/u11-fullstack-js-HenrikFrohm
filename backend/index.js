a7f49b90996d208bfe7a940b75f7f981c438950b
Henrik Frohm <woxer123@gmail.com>
2021-09-23 13:45:05 +0200

    Initial commit. Reinstalled project after encountering several issues in last version.

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+// initialize application, allowing methods
+const app = express();
+
+// allowing requests to be sent properly.
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// allows access to resources outside domain
+app.use(cors());
+
+// MongoDB database, when connected to Heroku the env will be populate env
+const CONNECTION_URL = 'mongodb+srv://u11user:u11pass123@cluster0.c8nhz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+// connect to database with options to avoid warnings. If connection is successful console will display message, otherwise error message.
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// doesn't work for latest version, so it's skipped
+//mongoose.set('useFindAndModify', false);
