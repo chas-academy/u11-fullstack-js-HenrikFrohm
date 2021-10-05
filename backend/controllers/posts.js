5a8ff332a85534db91f8d1db2d318ce2739613ed
Henrik Frohm <woxer123@gmail.com>
2021-10-05 11:16:30 +0200

    syntax error fixed to make updating posts work properly.

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.js';
+import mongoose from 'mongoose';
 
 // Functions, some asynchronous, for routes, separating logic from routes/posts.js to avoid clutter when more routes get added.
 // Appropriate HTTP status code responses shows up based on try-catch statements.
@@ -24,14 +25,14 @@ export const createPost = async (req, res) => {
         res.status(409).json({ message: error.message });
         
     }
-    res.send('Create Post');
 }
 
+// extracting renamed id from requered parameters. Checking if _id is a mongoose object. If it is  an async action goes through to get updated post that also gets updated in db. If not, 404 error. 
 export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body;
 
-    if(moongose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
 
