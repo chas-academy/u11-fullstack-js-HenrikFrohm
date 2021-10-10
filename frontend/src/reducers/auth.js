63807933cda19d53a6e5f4f854a127f433c77b1f
Henrik Frohm <woxer123@gmail.com>
2021-10-10 15:20:23 +0200

    if login is successful, data will be stored in localstorage

--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,10 +1,11 @@
 // reducer function for auth actions
 // giving state default value of null
+//if log in is successful, data will be stored in localstorage
 const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case "AUTH":
-      console.log(action?.data);
-      return state;
+      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      return { ...state, authData: action?.data };
     default:
       return state;
   }
