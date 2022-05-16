11f62ee325e23947744ca671b8511526fd65c35b
Henrik Frohm <woxer123@gmail.com>
2022-05-16 13:53:46 +0200

    added correct syntax and code to route post.js file

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import express from 'express';
-<<<<<<< HEAD
-
-const router = express.Router();
-
-// express methods to define routing corresponding to HTTP request methods like get and post.
-router.get('/', (req, res) => {
-    res.send('SUCCESS!');
-});
-=======
-import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
-
-const router = express.Router();
-
-// Defining router using express methods that correspond to HTTP methods, like get, post and update/patch.
-router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
->>>>>>> develop
-=======
 import express from "express";
 import {
   getPost,
@@ -43,6 +20,5 @@ router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
 router.patch("/:id/likePost", auth, likePost);
->>>>>>> develop
 
 export default router;
